f01d39560a9cbf444d187013aee13d59ea606761
ADITYA KUMAR <CodifyWithAditya@Gmail.com>
2024-02-19 15:42:57 +0530

    tble ui changes in order schedule tab done...adi 19feb24

--- src/pages/Sales/Orders/Components/ScheduleCreationForm/Tabs/ScheduleList/ScheduleList.jsx
+++ src/pages/Sales/Orders/Components/ScheduleCreationForm/Tabs/ScheduleList/ScheduleList.jsx
@@ -3,13 +3,15 @@ import { Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { postRequest } from "../../../../../../api/apiinstance";
 import { endpoints } from "../../../../../../api/constants";
+import FirstTable from "./Tables/FirstTable";
+import SecondTable from "./Tables/SecondTable";
 // import { Link, useNavigate } from "react-router-dom";
 // import { Tab, Table, Tabs, Form } from "react-bootstrap";
 
 export default function ScheduleList({ OrderData, OrderCustData }) {
-  console.log(OrderCustData);
+  // console.log(OrderCustData);
 
-  console.log(OrderData);
+  // console.log(OrderData);
 
   //date format
   const formatDate = (dateString) => {
@@ -35,7 +37,7 @@ export default function ScheduleList({ OrderData, OrderCustData }) {
       );
     }
   }, [OrderData]); // Watch for changes in OrderData
- 
+
   // useEffect(() => {
   //   ScheduleListData();
   // }, []);
@@ -56,160 +58,51 @@ export default function ScheduleList({ OrderData, OrderCustData }) {
     );
   };
 
-  console.log("DwgNameList is",DwgNameList);
+  // console.log("DwgNameList is", DwgNameList);
 
-  const onClickofScheduled=()=>{
-    console.log("response")
-  }
+  // const onClickofScheduled = () => {
+  //   console.log("response");
+  // };
 
   return (
     <>
       <div>
-        <div className="row mt-3">
-          <div className="col-md-2 col-sm-12">
+        <div className="row d-flex justify-content-center p-2">
+          <div className="col-md-2">
             <button
-              className="button-style "
+              className="button-style m-0"
               // onClick={openModal}
             >
               Delete Schedule
             </button>
           </div>
-          <div className="col-md-2 col-sm-12">
+          <div className="col-md-2">
             <Link
               to={"/Orders/Service/ServiceOpenSchedule"}
-              state=
-              {DwgNameList}
+              state={DwgNameList}
             >
-              <button className="button-style">Open Schedule</button>
+              <button className="button-style m-0">Open Schedule</button>
             </Link>
           </div>
         </div>
 
-        <div className="row mt-3">
-          <div className="col-md-3 col-md-12" style={{ overflowY: "scroll" }}>
-            <Table
-              striped
-              className="table-data border "
-              style={{ border: "1px", height: "400px" }}
-            >
-              <thead className="tableHeaderBGColor">
-                <tr>
-                  <th>Type</th>
-                  <th>No</th>
-                  <th>Status</th>
-                  <th>Delivered</th>
-                </tr>
-              </thead>
-              <tbody className="tablebody">
-                {scheduleListData.map((item, key) => {
-                  return (
-                    <>
-                      <tr
-                        onClick={() => onRowClickScheduleList(item, key)}
-                        className={
-                          key === rowScheduleList?.index
-                            ? "selcted-row-clr"
-                            : ""
-                        }
-                      >
-                        <td>{item.Type}</td>
-                        <td>{item.ScheduleNo}</td>
-                        <td>{item.Schedule_Status}</td>
-                        <td>{formatDate(item.Delivery_Date)}</td>
-                      </tr>
-                    </>
-                  );
-                })}
-                {/* {ordDetsData.length > 0 ? (
-                        ordDetsData.map((orddets, index) => {
-                          return (
-                            <tr
-                              key={orddets.index}
-                              onClick={() => selectItem(orddets)}
-                            >
-                              <td>{orddets["ScheduleType"]}</td>
-                              <td>{orddets["ScheduleNo"]}</td>
-                              <td>{orddets["Schedule_Status"]}</td>
-                              <td>{orddets["Delivery_Date"]}</td>
-                              <td>{orddets["ScheduleId"]}</td>
-                            </tr>
-                          );
-                        })
-                      ) : (
-                        <tr>
-                          <td colspan={5}>No Items Added</td>
-                        </tr>
-                      )} */}
-              </tbody>
-            </Table>
+        <div className="row">
+          <div
+            className="col-md-6"
+            style={{ height: "400px", overflow: "auto" }}
+          >
+            <FirstTable
+              scheduleListData={scheduleListData}
+              onRowClickScheduleList={onRowClickScheduleList}
+              rowScheduleList={rowScheduleList}
+              formatDate={formatDate}
+            />
           </div>
-          <div className="col-md-9 col-md-12" style={{ overflowY: "scroll" }}>
-            <Table
-              striped
-              className="table-data border mt-2"
-              style={{
-                border: "1px",
-                height: "400px",
-                overflowY: "scroll",
-              }}
-            >
-              <thead className="tableHeaderBGColor">
-                <tr>
-                  <th>Dwg Name</th>
-                  <th>Mtrl Code</th>
-                  <th>Operation</th>
-                  <th>Scheduled</th>
-                  <th>Produced</th>
-                  <th>Packed</th>
-                  <th>Delivered</th>
-                  <th>JW Cost</th>
-                  <th>Mtrl Cost</th>
-                </tr>
-              </thead>
-              <tbody className="tablebody">
-                {DwgNameList.map((item, key) => {
-                  return (
-                    <>
-                      <tr>
-                        <td>{item.DwgName}</td>
-                        <td>{item.Mtrl_Code}</td>
-                        <td>{item.Operation}</td>
-                        <td>{item.QtyScheduled}</td>
-                        <td>{item.QtyProduced}</td>
-                        <td>{item.QtyPacked}</td>
-                        <td>{item.QtyDelivered}</td>
-                        <td>{item.JWCost}</td>
-                        <td>{item.MtrlCost}</td>
-                      </tr>
-                    </>
-                  );
-                })}
-                {/* {ordDetsDwgData.length > 0 ? (
-                        ordDetsDwgData.map((orddetsdwg, index) => {
-                          return (
-                            <tr
-                              key={orddetsdwg.index}
-                              onClick={() => selectItem(orddetsdwg)}
-                            >
-                              <td>{orddetsdwg["DwgName"]}</td>
-                              <td>{orddetsdwg["Mtrl_Code"]}</td>
-                              <td>{orddetsdwg["Operation"]}</td>
-                              <td>{orddetsdwg["QtyScheduleId"]}</td>
-                              <td>{orddetsdwg["QtyProduced"]}</td>
-                              <td>{orddetsdwg["QtyPacked"]}</td>
-                              <td>{orddetsdwg["QtyDelivered"]}</td>
-                              <td>{orddetsdwg["JWCost"]}</td>
-                              <td>{orddetsdwg["MtrlCost"]}</td>
-                            </tr>
-                          );
-                        })
-                      ) : (
-                        <tr>
-                          <td colspan={9}>No Items Added</td>
-                        </tr>
-                      )} */}
-              </tbody>
-            </Table>
+          <div
+            className="col-md-6"
+            style={{ height: "400px", overflow: "auto" }}
+          >
+            <SecondTable DwgNameList={DwgNameList} />
           </div>
         </div>
         {/* <AlertModal
