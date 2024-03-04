c596a786ca8f3e5ce15c703430b0614f5025e436
Prakruthi Holla <prakruthi.holla@ahanait.com>
2024-03-04 15:55:35 +0530

    changes of ordeschno

--- src/pages/Sales/Orders/Menus/Service/ScheduleCreationForm/BodyTabs/ScheduleList/ServiceOpenSchedule.js
+++ src/pages/Sales/Orders/Menus/Service/ScheduleCreationForm/BodyTabs/ScheduleList/ServiceOpenSchedule.js
@@ -411,8 +411,9 @@ function ServiceOpenSchedule() {
 
   // console.log("fixturedata is",fixturedata);
 
-  const Orsch = formdata[0]?.Order_No + " " + formdata[0]?.ScheduleNo;
-
+  const ScheduleNo = formdata[0]?.ScheduleNo;
+  const Orsch = formdata[0]?.Order_No + " " + (ScheduleNo != null && ScheduleNo !== 'null' && ScheduleNo !== undefined ? ScheduleNo : '');
+  
   //CHECK STATUS
   const checkstatus=()=>{
     postRequest(
